c9ed8d8116349475c5e2d1c96c28dcdfe22d7097
mkassjanski <ankoupl80@gmail.com>
2017-04-02 16:37:36 +0200

    pierwsze zadania

---
+++ zad_str_65.py
@@ -0,0 +1,40 @@
+#Zad1
+'''
+pieniadze = 1000
+if pieniadze > 1000:
+    print("Jestem osoba bogata")
+else:
+    print("Nie jestem osoba bogata")
+    print("Ale moze w przyszlosci")
+'''
+
+#Zad2
+'''
+wafelki=1000
+if(wafelki<100):
+    print("Za malo")
+elif(wafelki>500):
+    print("Za duzo")
+'''
+
+#Zad 3
+'''
+kwota=342
+if(kwota>=100 and kwota<=500):
+    print("miedzy 100 a 500")
+elif(kwota>=1000 and kwota<=5000):
+    print("miedzy 1000 a 5000")
+else:
+    print("poza wskazanymi przedzialami")
+'''
+
+#Zad 4
+"""
+ninja=5
+if(ninja<10):
+    print("Dajecie mi tych mieszkancow ninja!")
+elif(ninja<30):
+    print("Poleje sie krew, ale dam sobie rade")
+elif(ninja<50):
+    print("To zbyt wiele")
+"""
